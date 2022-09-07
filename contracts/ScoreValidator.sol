41eaf59c8a374ea3c9cf47efae739abf280c3144
cehali <cezary.haliniarz@gmail.com>
2022-09-07 18:09:48 +0200

    chore: update contract

--- contracts/ScoreValidator.sol
+++ contracts/ScoreValidator.sol
@@ -5,16 +5,35 @@ import "redstone-evm-connector/lib/contracts/message-based/PriceAware.sol";
 
 contract ScoreValidator is PriceAware {
   uint256 minimalScoreRequired;
+
   constructor(uint256 _minimalScoreRequired) {
     minimalScoreRequired = _minimalScoreRequired;
   }
 
-  function isSignerAuthorized(address _receviedSigner) public override virtual view returns (bool) {
+  function isSignerAuthorized(address _receviedSigner)
+    public
+    view
+    virtual
+    override
+    returns (bool)
+  {
     return _receviedSigner == 0x0C39486f770B26F5527BBBf942726537986Cd7eb; // redstone-main
   }
 
-  function checkCreditscore(bytes32 lensHandle) view public returns (uint256, uint256, bool) {
+  function checkCreditScore(bytes32 lensHandle)
+    public
+    view
+    returns (
+      uint256,
+      uint256,
+      bool
+    )
+  {
     uint256 score = getPriceFromMsg(lensHandle);
-    return (minimalScoreRequired, score, (score / 10 ** 8) >= minimalScoreRequired);
+    return (
+      minimalScoreRequired,
+      score,
+      (score / 10**8) >= minimalScoreRequired
+    );
   }
 }
